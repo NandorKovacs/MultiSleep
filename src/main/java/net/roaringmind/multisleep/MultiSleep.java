3be4cefc4919ba6502ef7d0121baf2d45cdb1e59
NandorKovacs <nandor1kovacs@gmail.com>
2021-05-18 10:13:25 +0200

    /multisleep forcesleep working

--- src/main/java/net/roaringmind/multisleep/MultiSleep.java
+++ src/main/java/net/roaringmind/multisleep/MultiSleep.java
@@ -1,44 +1,16 @@
 package net.roaringmind.multisleep;
 
-import static net.minecraft.server.command.CommandManager.literal;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.stat.StatFormatter;
-import net.minecraft.stat.Stats;
-import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.GameRules.BooleanRule;
-import net.minecraft.world.GameRules.Category;
-import net.minecraft.world.GameRules.IntRule;
-import net.minecraft.world.GameRules.Key;
-import net.roaringmind.multisleep.callbacks.ButtonClickCallback;
-import net.roaringmind.multisleep.callbacks.PhantomClickCallback;
-import net.roaringmind.multisleep.callbacks.PlayerSleepCallback;
-import net.roaringmind.multisleep.callbacks.PlayerTickCallback;
-import net.roaringmind.multisleep.callbacks.VoteClickCallback;
-import net.roaringmind.multisleep.callbacks.WorldSleepCallback;
 
 public class MultiSleep implements ModInitializer {
 
@@ -50,6 +22,27 @@ public class MultiSleep implements ModInitializer {
   @Override
   public void onInitialize() {
     log(Level.INFO, "Initializing");
+
+    registerCommands();
+  }
+
+  private void registerCommands() {
+    CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+      dispatcher.register(literal("multisleep")
+        .then(literal("forcesleep")
+          .executes(ctx -> {
+            sleep();
+            return 0;
+          })  
+        )
+      );
+    });
+  }
+
+  public static boolean shoudlSleepNow = false;
+
+  private void sleep() {
+    shoudlSleepNow = true;
   }
 
   public static void log(Level level, String message) {
---
+++ src/main/java/net/roaringmind/multisleep/mixin/ServerSleepMixin.java
@@ -0,0 +1,61 @@
+package net.roaringmind.multisleep.mixin;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
+
+import org.apache.logging.log4j.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.MutableWorldProperties;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+import net.roaringmind.multisleep.MultiSleep;
+
+@Mixin(ServerWorld.class)
+public abstract class ServerSleepMixin extends World {
+
+  ServerSleepMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, final DimensionType dimensionType,
+      Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed) {
+    super(properties, registryRef, dimensionType, profiler, isClient, debugWorld, seed);
+  }
+
+  @Shadow
+  private boolean allPlayersSleeping;
+
+  @Shadow
+  public abstract void setTimeOfDay(long timeOfDay);
+
+  @Shadow
+  public abstract void wakeSleepingPlayers();
+
+  @Shadow
+  public abstract void resetWeather();
+
+  @Inject(method = "tick", at = @At(value = "RETURN", args = { "log = true" }))
+  public void sleepInject(BooleanSupplier shouldKeepTicking, CallbackInfo cir) {
+    if (!MultiSleep.shoudlSleepNow) {
+      return;
+    }
+
+    MultiSleep.log(Level.INFO, "will sleep now");
+
+    if (this.getGameRules().getBoolean(GameRules.DO_DAYLIGHT_CYCLE)) {
+      long l = this.properties.getTimeOfDay() + 24000L;
+      this.setTimeOfDay(l - l % 24000L);
+    }
+
+    this.wakeSleepingPlayers();
+    if (this.getGameRules().getBoolean(GameRules.DO_WEATHER_CYCLE)) {
+      this.resetWeather();
+    }
+    MultiSleep.shoudlSleepNow = false;
+  }
+}
