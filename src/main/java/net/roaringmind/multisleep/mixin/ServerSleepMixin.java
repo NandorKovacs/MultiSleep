dc698067a276f29ed060119dc99e6f0ba1a28523
NandorKovacs <nandor1kovacs@gmail.com>
2021-05-26 18:04:53 +0200

    Translatable keybinds

--- src/main/java/net/roaringmind/multisleep/mixin/ServerSleepMixin.java
+++ src/main/java/net/roaringmind/multisleep/mixin/ServerSleepMixin.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.RegistryKey;
