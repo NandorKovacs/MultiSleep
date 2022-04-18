7b8cfefe44133d7d889f6899836227e92b58b6b3
NandorKovacs <nandor1kovacs@gmail.com>
2022-04-18 15:51:51 +0200

    @At import correct

--- src/main/java/net/roaringmind/multisleep/mixin/SleepingChatScreenMixin.java
+++ src/main/java/net/roaringmind/multisleep/mixin/SleepingChatScreenMixin.java
@@ -1,12 +1,11 @@
 package net.roaringmind.multisleep.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.SleepingChatScreen;
-import net.minecraft.entity.player.PlayerEntity;
 import net.roaringmind.multisleep.callbacks.ExitBedCallback;
 
 @Mixin(SleepingChatScreen.class)
